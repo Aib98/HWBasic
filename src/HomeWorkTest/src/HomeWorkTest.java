f30453a20ac233844fbbf772faedddfb4d694160
Andrey B <aib98339@gmail.com>
2022-05-06 16:04:47 +0300

    ДЗ с помощью классов. Не работает сумма правильных и пеправильных ответов

---
+++ src/HomeWorkTest/src/HomeWorkTest.java
@@ -0,0 +1,16 @@
+
+import java.util.Scaner;
+
+public class HomeWorkTest {
+    public static void main(String[] args) {
+        Question answer = new Question();
+
+        answer.quest = new String[][]{
+                {"В файл с каким расширением компилируется java-файл?", "1. cs", "2. java", "3. class", "4. exe"},
+                {"С помощью какой команды git можно получить полную копию удаленного репозитория?", "1. commit", "2. push", "3. clone", "4. copy"},
+                {"С помощью какой команды git можно получить полную копию удаленного репозитория?", "1. While", "2. for", "3. loop"}
+        };
+
+
+    }
+}
