c4ee7d4de1dee7b84935ac01ba16a761e52b5a77
Andrey B <aib98339@gmail.com>
2022-05-13 15:09:53 +0300

    Пробный вариант ДЗ по сортировки 3 методов с оценкой скорости

--- src/Sorting/src/Sort.java
+++ src/Sorting/src/Sort.java
@@ -1,3 +1,4 @@
+import java.io.OptionalDataException;
 import java.net.SocketOption;
 import java.security.spec.RSAOtherPrimeInfo;
 import java.time.Duration;
@@ -10,40 +11,29 @@ import java.util.concurrent.TimeUnit;
 
 public class Sort {
     public static void main (String[] args) throws InterruptedException {
-//        int[] massive = new int[] {9, 3, 5, 1, 8, 12, 7};
-        Instant start =Instant.now();// Запускаем таймер 1
-
         System.out.print("Рандомный массив: ");
-
         int massive[] = new int[10]; // создаем рандомный массив из 10 целых чисел + печатаем их
         for (int q = 0; q < massive.length; q++) {
             massive[q] = (int)(Math.random()*100);
             System.out.print(massive[q] + " ");
         }
-//        for (int j : massive) {
-//            System.out.print(j + " ");
-//        }
         System.out.println();
 
         System.out.print("Сортированный массив с помощью метода sort(): ");
+        long start3 = System.nanoTime();
         Arrays.sort(massive);// Сортированный массив с помощью метода sort
         for (int w = 0; w < massive.length; w++) {
             System.out.print(massive[w] + " "); // Выводим на экран сортированные числа с помощью метода sort()
         }
+        Thread.sleep(1000);
+        long finish3 = System.nanoTime();
         System.out.println();
 
-        Thread.sleep(1000);
-        Instant finish = Instant.now(); // Останавливаем таймер 1
-        long speed = Duration.between(start, finish).toMillis(); // Рассчитываем время
-        System.out.println("Время работы метода sort: " + speed); // Выводим на экран в мс
+        Long millis = finish3 - start3;
 
-//        final long then = System.nanoTime();
-//        TimeUnit.SECONDS.sleep(1);
-//        final  long millis = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - then);
-//        System.out.println("Время работы: " + millis );
+        System.out.println("Время работы метода Sort: " + millis );
 
-
-        Instant start2 =Instant.now(); // Запускаем таймер 2
+        Long start4 = System.nanoTime();
         int temp;
         System.out.print("Сортированный массив методом пузырика: " );
         for (int k = massive.length - 1; k > 0; k--) {
@@ -55,21 +45,47 @@ public class Sort {
                 }// Сортируем с помощью "пузырика"
             }
         }
+        Thread.sleep(1000);
+        long finish4 = System.nanoTime();
         for (int j : massive) {
             System.out.print(j + " ");
         }
         System.out.println();
+        long millis2 = finish4 - start4;
 
-//        Thread.sleep(1000);
-//        Instant finish2 = Instant.now(); // Останавливаем таймер 2
-//        long speed2 = Duration.between(start, finish2).toMillis(); //Рассчитываем время 2
-//        System.out.println("Время работы метода sort: " + speed);
+        System.out.println("Время работы метода Пузырика: " + millis2 );
+        System.out.println("Сравнение методов Пузырика и Start: " +(millis2 - millis));
 
+        long start5 = System.nanoTime();// Запускаем таймер метода Выбора
+        Thread.sleep(1000);
+        System.out.print("Сортированный массив методом Выбора: " );
+        for (int i =0; i < massive.length; i++){
+            int min = massive[i];
+            int minId = i;
+            for (int a = i+1; a < massive.length; a++) {
+                if (massive[a]< min){
+                    min = massive[a];
+                    minId = a;
+                }
+            }
+        }
+        long finish5 = System.nanoTime();
+        long millis3 = finish5 - start5;
 
-        final long then2 = System.nanoTime();
-        TimeUnit.SECONDS.sleep(1);
-        final  long millis2 = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - then2);
-        System.out.println("Время работы: " + millis2 );
-        System.out.println("Сравнение методов: " +(speed - millis2));
+        for (int w : massive) {
+            System.out.print(w + " ");
+        }
+        System.out.println();
+        System.out.println("Время работы метода Выбора: " + millis3 );
+        System.out.println("Сравнение методов Выбора и Start: " +(millis3 - millis));
+        System.out.println("Сравнение методов Выбора и Пузырика: " +(millis3 - millis2));
+
+        long[] massiveMillis = new long[] {millis, millis2, millis3};
+
+        Arrays.sort(massiveMillis);// Сортированный массив с помощью метода sort
+        System.out.println("Сортируем методы: ");
+        for (int r = 0; r < massiveMillis.length; r++) {
+            System.out.print(massiveMillis[r] + " "); // Выводим на экран сортированные числа с помощью метода sort()
+        }
     }
 }
